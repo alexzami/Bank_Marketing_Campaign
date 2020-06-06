2e7c8202ec45f2faa7579b6938908eb4acbe6ecc
Pavlos Floratos <pfloratos@csd.auth.gr>
2020-06-06 13:01:51 +0300

    First version of project

---
+++ bank2.py
@@ -0,0 +1,63 @@
+'''
+Created on 10 May 2020
+
+@author: 30693
+'''
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder,MinMaxScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split,GridSearchCV
+from sklearn.pipeline import Pipeline
+
+
+bank = pd.read_csv(r'C:\Users\30693\Desktop\bank\bank-additional-full.csv', sep=';')
+bank_X = bank.iloc[:,[i for i in range(0,len(bank.columns)-1)]]
+bank_X = bank_X.drop('duration', axis=1)
+bank_y = bank.iloc[:,-1]
+
+
+all_columns = bank_X.columns
+numerical_cols = bank_X._get_numeric_data().columns
+categorial_columns = list(set(all_columns) - set(numerical_cols))
+
+for cat in categorial_columns:
+    bank_X = pd.concat([bank_X.drop(cat, axis=1), pd.get_dummies(bank_X[cat],prefix=cat, drop_first = True)], axis=1)
+ 
+bank_y = pd.get_dummies(bank_y, prefix = ['y'], drop_first = True)
+
+
+x_train, x_test, y_train, y_test = train_test_split(bank_X, bank_y, stratify = bank_y, train_size = 0.7, random_state = 0)
+
+scaler = MinMaxScaler()
+    
+logmodel = LogisticRegression(max_iter = 600) 
+pipeline = Pipeline(steps = [('scaler',scaler), ('classifier',logmodel)])
+
+param_grid = {'scaler__feature_range':[(0,1)]}
+
+grid_search  = GridSearchCV(pipeline, param_grid=param_grid, cv = 10, scoring="f1", n_jobs=-1, iid = False, verbose=1)
+grid_search.fit(x_train,y_train.values.ravel())
+y_pred = grid_search.predict(x_test)
+
+print(grid_search.best_estimator_)
+print(grid_search.best_score_)
+print(grid_search.refit_time_)
+
+
+accuracy = metrics.accuracy_score(y_test,y_pred)
+precision = metrics.precision_score(y_test,y_pred)
+recall = metrics.recall_score(y_test,y_pred)
+f1 = metrics.f1_score(y_test,y_pred)
+ 
+ 
+print("Recall: "+str(round((recall*100),2))+"%")
+print("Precision: "+str(round((precision*100),2))+"%")
+print("Accuracy: "+str(round((accuracy*100),2))+"%")
+print("F1: "+str(round((f1*100),2))+"%")
